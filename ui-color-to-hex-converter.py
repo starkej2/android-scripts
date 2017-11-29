06b4b4daa7d258e0cefbf1634634ef602116665f
Jeffrey Starke <starkej2@gmail.com>
2017-11-29 10:48:57 -0500

    UI color to HEX converter readability improvements

--- ui-color-to-hex-converter.py
+++ ui-color-to-hex-converter.py
@@ -5,6 +5,12 @@
 
 SPACE_CHAR = " "
 MAX_COLOR_VALUE = 255
+MAX_ALPHA_IN_HEX = "ff"
+
+UI_COLOR_RED_PREFIX = "red:"
+UI_COLOR_GREEN_PREFIX = "green:"
+UI_COLOR_BLUE_PREFIX = "blue:"
+UI_COLOR_ALPHA_PREFIX = "alpha:"
 
 
 def find_attribute(ui_color_string, prefix):
@@ -14,19 +20,19 @@ def find_attribute(ui_color_string, prefix):
 
 
 def get_red(ui_color_string):
-    return float(find_attribute(ui_color_string, "red:"))
+    return float(find_attribute(ui_color_string, UI_COLOR_RED_PREFIX))
 
 
 def get_green(ui_color_string):
-    return float(find_attribute(ui_color_string, "green:"))
+    return float(find_attribute(ui_color_string, UI_COLOR_GREEN_PREFIX))
 
 
 def get_blue(ui_color_string):
-    return float(find_attribute(ui_color_string, "blue:"))
+    return float(find_attribute(ui_color_string, UI_COLOR_BLUE_PREFIX))
 
 
 def get_alpha(ui_color_string):
-    return float(find_attribute(ui_color_string, "alpha:"))
+    return float(find_attribute(ui_color_string, UI_COLOR_ALPHA_PREFIX))
 
 
 def convert_to_hex(ui_color_value):
@@ -42,6 +48,7 @@ while True:
     blue_hex_value = convert_to_hex(get_blue(ui_color_input))
     alpha_hex_value = convert_to_hex(get_alpha(ui_color_input))
 
-    print "HEX representation: #{hex}".format(
-        hex=red_hex_value + green_hex_value + blue_hex_value if alpha_hex_value == "ff"
-        else alpha_hex_value + red_hex_value + green_hex_value + blue_hex_value)
+    full_hex = red_hex_value + green_hex_value + blue_hex_value if alpha_hex_value == MAX_ALPHA_IN_HEX \
+        else alpha_hex_value + red_hex_value + green_hex_value + blue_hex_value
+
+    print "HEX representation: #{hex}".format(hex=full_hex)
