8cc89ee8203e010071a29cc50f061459e8f9092e
Jeffrey Starke <starkej2@gmail.com>
2018-01-12 11:36:10 -0500

    Create clear-app-data bash script (WIP)

---
+++ clear-app-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# TODO add documentation
+# TODO read in app package and launch activity class parameters
+
+# clear app data
+#adb shell pm clear <app-package-name>
+
+# launch app
+#adb shell am start -n <app-package-name>/<qualified-launch-activity-class>
